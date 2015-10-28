b94dd5d0821fb4df890f1b1de8bd633aecb35001
Justin Chang <changj41@gmail.com>
2015-10-28 23:26:56 +0800

    Initial Adjustable Grid

---
+++ Project Grid/Assets/GameController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+  public GameObject _white;
+  public GameObject _blue;
+
+  public int _width;
+  public int _length;
+
+  private bool alternate = false;
+
+	// Use this for initialization
+	void Start () {
+
+    for(int i = 0; i < _width*_length; i++)
+    {
+      Quaternion initQuat = Quaternion.Euler(new Vector3(90, 0, 0));
+      Vector3 initPosition = new Vector3((int) i / _width, 0, (int) i % _width);
+      if(i % _width == 0) {
+        alternate = !alternate;
+      }
+
+      if(alternate) {
+        if(i % 2 == 0) {
+          Instantiate(_white, initPosition, initQuat);
+        } else {
+          Instantiate(_blue, initPosition, initQuat);
+        }
+      } else {
+        if(i % 2 == 0) {
+          Instantiate(_blue, initPosition, initQuat);
+        } else {
+          Instantiate(_white, initPosition, initQuat);
+        }
+      }
+
+    }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
