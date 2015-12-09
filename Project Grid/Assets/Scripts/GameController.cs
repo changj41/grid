d02b14ad1cb3a926e0706fc5f6eccf178a5ab175
Justin Chang <changj41@gmail.com>
2015-12-09 18:19:03 +0800

    GameController to start controlling everything

--- Project Grid/Assets/Scripts/GameController.cs
+++ Project Grid/Assets/Scripts/GameController.cs
@@ -3,17 +3,43 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+  private InputManager _inputManager;
+  private GameObject _camera;
+
+  private bool turnBeginPhase;
+  private bool characterSelectPhase;
+  private bool movementPhase;
+  private bool attackPhase;
+
   public bool pieceSelected;
   public string selectedUnit;
 
-	// Use this for initialization
-	void Start () {
-    pieceSelected = false;
+  //Temp
+  private GameObject summoner;
+  private GameObject hero;
 
+  void Start ()
+  {
+    _inputManager = new InputManager();
+    _camera = GameObject.FindGameObjectWithTag(Constants.Tags.MainCamera);
+    pieceSelected = false;
+    turnBeginPhase = false;
+    characterSelectPhase = false;
+    movementPhase = false;
+
+    //Temp
+    summoner = GameObject.Find("Summoner");
+    hero = GameObject.Find("Hero");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	  
+
+	void Update ()
+  {
+    ScreenInput input = _inputManager.GetInput();
+    if(input != null)
+    {
+      Ray ray = new Ray(_camera.transform.position, input.inputPoint - _camera.transform.position);
+      RaycastHit[] hits = Physics.RaycastAll(ray, Constants.Board.raycastLength);
+    }
 	}
 }
